1b032aaa03e0d038a139e31ac2350dbb617c71f8
0xukhezo <ukhezo.web3@gmail.com>
2022-11-10 11:29:21 +0100

    Get usdc price and user data functions

--- scripts/borrow.js
+++ scripts/borrow.js
@@ -13,6 +13,17 @@ async function main() {
     console.log("Depositing...")
     await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
     console.log("Deposited!")
+    let { availableBorrowsETH, totalDebtETH } = await getBorrowUserData(
+        lendingPool,
+        deployer
+    )
+    const usdcPrice = await getUsdcPrice()
+    const amountUsdcToBorrow =
+        availableBorrowsETH.toString() * 0.95 * (1 / Number(usdcPrice))
+    console.log(`You can borrow ${amountUsdcToBorrow} USDC`)
+    const amountUsdcToBorrowWei = ethers.utils.parseEther(
+        amountUsdcToBorrow.toString()
+    )
 }
 
 async function getLendingPool(account) {
@@ -46,6 +57,26 @@ async function approveERC20(
     const tx = await erc20Token.approve(spenderAddress, amountToSpend)
     await tx.wait(1)
     console.log("Approved!")
+}
+
+async function getBorrowUserData(lendingPool, account) {
+    const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+        await lendingPool.getUserAccountData(account)
+    console.log(`You have ${totalCollateralETH} worth of ETH deposited`)
+    console.log(`You have ${totalDebtETH} worth of ETH borrow`)
+    console.log(`You can borrow ${availableBorrowsETH} worth of ETH`)
+    return { availableBorrowsETH, totalDebtETH }
+}
+
+async function getUsdcPrice() {
+    const usdcEth = "0x986b5E1e1755e3C2440e960477f25201B0a8bbD4"
+    const daiEthPriceFeed = await ethers.getContractAt(
+        "AggregatorV3Interface",
+        usdcEth
+    )
+    const price = (await daiEthPriceFeed.latestRoundData())[1]
+    console.log(`The USDC ETH price is ${price.toString()}`)
+    return price
 }
 
 main()
