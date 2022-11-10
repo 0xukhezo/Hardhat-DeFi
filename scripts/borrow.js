a9eb9479a74ded27c2913ee0d3337b35f82fbc6f
0xukhezo <ukhezo.web3@gmail.com>
2022-11-10 10:50:59 +0100

    Approve and getlendingPool functions

--- scripts/borrow.js
+++ scripts/borrow.js
@@ -4,6 +4,48 @@ const { networkConfig } = require("../helper-hardhat-config")
 
 async function main() {
     await getWeth()
+    const { deployer } = await getNamedAccounts()
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`Lending Pool address: ${lendingPool.address}`)
+
+    const wethTokenAddress = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+    await approveERC20(wethTokenAddress, lendingPool.address, AMOUNT, deployer)
+    console.log("Depositing...")
+    await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
+    console.log("Deposited!")
+}
+
+async function getLendingPool(account) {
+    const lendingPoolAddressesProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        networkConfig[network.config.chainId].lendingPoolAddressesProvider,
+        account.address
+    )
+    const lendingPoolAddress =
+        await lendingPoolAddressesProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt(
+        "ILendingPool",
+        lendingPoolAddress,
+        account.address
+    )
+    return lendingPool
+}
+
+async function approveERC20(
+    erc20Address,
+    spenderAddress,
+    amountToSpend,
+    account
+) {
+    const erc20Token = await ethers.getContractAt(
+        "IERC20",
+        erc20Address,
+        account.address
+    )
+
+    const tx = await erc20Token.approve(spenderAddress, amountToSpend)
+    await tx.wait(1)
+    console.log("Approved!")
 }
 
 main()
